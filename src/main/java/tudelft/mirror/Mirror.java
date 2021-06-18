9f32017926a1e3c7fa740b14f8cff079e3df0fa5
Alessandro <lacu.alessandro@gmail.com>
2021-06-18 22:46:24 +0200

    final

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -3,19 +3,19 @@ package tudelft.mirror;
 public class Mirror {
 
     public String mirrorEnds(String string) {
-        String mirror = "";
+        StringBuilder mirror = new StringBuilder();
 
         int begin = 0;
         int end = string.length() - 1;
         for (; begin < end; begin++, end--) {
             if (string.charAt(begin) == string.charAt(end)) {
-                mirror += String.valueOf(string.charAt(end));
+                mirror.append(string.charAt(end));
             }
             else {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        return begin == end ? string : mirror.toString();
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,27 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "abXYZba,ab",
+                    "abca,a",
+                    "aba,aba",
+                    "abba,ab",
+                    "abc,",
+                    "a,a",
+                    "aa,a"
+            }
+    )
+    void testMirror(String input, String expected){
+        if(expected==null)
+            expected="";
+        final String s = new Mirror().mirrorEnds(input);
+        Assertions.assertEquals(expected, s);
+    }
 }
